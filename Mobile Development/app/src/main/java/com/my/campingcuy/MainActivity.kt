d97310536cb79e25aa5dfb2a708888a087530f1a
wira07 <wiralodrasaputra07@gmail.com>
2023-12-05 14:21:42 +0700

    initial commit for md

---
+++ Mobile Development/app/src/main/java/com/my/campingcuy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.my.campingcuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
