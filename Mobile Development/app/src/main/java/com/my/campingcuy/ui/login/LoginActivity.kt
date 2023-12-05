014e3acf616c209a9cb3361aa48119b96bf03044
wira07 <wiralodrasaputra07@gmail.com>
2023-12-05 22:41:20 +0700

    update file

--- Mobile Development/app/src/main/java/com/my/campingcuy/ui/login/LoginActivity.kt
+++ Mobile Development/app/src/main/java/com/my/campingcuy/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.my.campingcuy
+package com.my.campingcuy.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.my.campingcuy.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ Mobile Development/app/src/main/java/com/my/campingcuy/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.my.campingcuy.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.my.campingcuy.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
