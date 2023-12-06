eabbbc5fcd83fcd910e939e9a51fa54e849c56ca
wira07 <wiralodrasaputra07@gmail.com>
2023-12-06 11:04:38 +0700

    Update Penambahan Soucode, penambahan strings.xml, color  dan penambahan rectangle_shape_3.xml

--- Mobile Development/app/src/main/java/com/my/campingcuy/ui/custom/EmailEditText.kt
+++ Mobile Development/app/src/main/java/com/my/campingcuy/ui/custom/EmailEditText.kt
@@ -1,4 +1,54 @@
 package com.my.campingcuy.ui.custom
 
-class EmailEditText {
+import android.content.Context
+import android.os.Build
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.util.Patterns
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatEditText
+import com.my.campingcuy.R
+
+class EmailEditText : AppCompatEditText {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun init() {
+        inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+        compoundDrawablePadding = 24
+
+        setHint(R.string.email)
+        setAutofillHints(AUTOFILL_HINT_EMAIL_ADDRESS)
+        importantForAccessibility
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty() && !Patterns.EMAIL_ADDRESS.matcher(s).matches())
+                    error = context.getString(R.string.error_email_is_not_valid)
+            }
+        })
+    }
 }
--- Mobile Development/app/src/main/java/com/my/campingcuy/ui/custom/PasswordEditText.kt
+++ Mobile Development/app/src/main/java/com/my/campingcuy/ui/custom/PasswordEditText.kt
@@ -1,4 +1,55 @@
 package com.my.campingcuy.ui.custom
 
-class PasswordEditText {
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatEditText
+import com.my.campingcuy.R
+
+class PasswordEditText : AppCompatEditText {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun init() {
+        inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+        compoundDrawablePadding = 24
+
+        setHint(R.string.password)
+        setAutofillHints(AUTOFILL_HINT_PASSWORD)
+        transformationMethod = PasswordTransformationMethod()
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty() && s.length < 8)
+                    error = context.getString(R.string.error_password_min_8_char)
+            }
+        })
+    }
 }
