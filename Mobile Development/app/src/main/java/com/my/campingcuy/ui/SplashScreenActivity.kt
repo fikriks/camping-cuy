3c97b08ed1a4a99a7142543d92ba75634d3db9a9
wira07 <wiralodrasaputra07@gmail.com>
2023-12-07 12:17:10 +0700

    mengganti SplashScreenActivity, Menambahkan Perizinan di SplashScreenActivity di manyfest

--- Mobile Development/app/src/main/java/com/my/campingcuy/ui/SplashScreenActivity.kt
+++ Mobile Development/app/src/main/java/com/my/campingcuy/ui/SplashScreenActivity.kt
@@ -1,11 +1,12 @@
-package com.my.campingcuy
+package com.my.campingcuy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.my.campingcuy.R
 
-class SplashActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
