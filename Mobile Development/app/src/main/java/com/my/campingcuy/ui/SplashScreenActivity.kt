4fa167e1bdb1d49b65834661ea3803d1c3beef58
wira07 <wiralodrasaputra07@gmail.com>
2023-12-08 15:18:27 +0700

    Menambahkan intent dan pindah halaman ke dari SplashScreenActivity ke LoginActivity dan menambahkan  android:theme="@style/Theme.MaterialComponents.Light" di manyfest

--- Mobile Development/app/src/main/java/com/my/campingcuy/ui/SplashScreenActivity.kt
+++ Mobile Development/app/src/main/java/com/my/campingcuy/ui/SplashScreenActivity.kt
@@ -1,14 +1,24 @@
 package com.my.campingcuy.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.my.campingcuy.R
+import com.my.campingcuy.databinding.ActivitySplashScreenBinding
+import com.my.campingcuy.ui.login.LoginActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
-    }
+        binding =  ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.button.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.putExtra("kunci", "Nilai yang akan Anda kirim")
+            startActivity(intent)
+        }
+    }
 
 }
